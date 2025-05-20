834a0ce10cdee8fe0e54651d615dd3a56ff326ce
Rizki Kotet <rizkidhc31@gmail.com>
2025-05-20 13:16:56 +0700

    init

--- files/usr/lib/lua/luci/controller/base64.lua
+++
@@ -1,4 +0,0 @@
-module("luci.controller.base64", package.seeall)
-function index()
-entry({"admin","services","base64"}, template("base64"), _("Base64 D/E"), 7).leaf=true
-end
--- files/usr/lib/lua/luci/controller/tinyfm.lua
+++
@@ -1,4 +0,0 @@
-module("luci.controller.tinyfm", package.seeall)
-function index()
-entry({"admin","system","tinyfm"}, template("tinyfm"), _("File Manager"), 47).leaf=true
-end
